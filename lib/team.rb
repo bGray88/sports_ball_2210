1f063448a41a7a2debceeffcc0cab857ebfa452f
Brandon Gray <brandonjoe88@hotmail.com>
2022-10-19 13:07:40 -0600

    feat: Update team for iteration 4

--- lib/team.rb
+++ lib/team.rb
@@ -2,17 +2,60 @@ require './lib/player'
 
 class Team
 
-  attr_reader :roster, :player_count
+  attr_reader :roster, :player_count,
+              :long_term_players, :short_term_players,
+              :total_value, :details,
+              :average_cost_of_player
 
   def initialize(name, city)
     @name = name
     @city = city
     @roster = []
     @player_count = 0
+    @long_term_players = []
+    @short_term_players = []
+    @total_value = 0
+    @details = {}
+    @average_cost_of_player = ""
+    @players_by_last_names = ""
   end
 
   def add_player(player)
     @roster << player
     @player_count += 1
+    term_length(player)
+    calculate_value(player)
+    set_details
+    calculate_average(player)
+  end
+
+  def term_length(player)
+    if player.contract_length > 24
+      @long_term_players << player
+    else
+      @short_term_players << player
+    end
+  end
+
+  def calculate_value(player)
+    @total_value += player.total_cost
+  end
+
+  def set_details
+    @details["total_value"] = @total_value
+    @details["player_count"] = @player_count
+  end
+
+  def calculate_average(player)
+    avg_num = @total_value / @player_count
+    avg_num = avg_num.to_s.chars.insert(-4, ',').join
+    avg_num = avg_num.to_s.chars.insert(-8, ',').join
+    @average_cost_of_player = "$#{avg_num}"
+  end
+
+  def players_by_last_name
+    last_names = roster.map do |player|
+      player.last_name
+    end.sort.join(', ')
   end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -26,4 +26,52 @@ describe Team do
     expect(team.roster).to eq([player_1, player_2])
     expect(team.player_count).to eq(2)
   end
+
+  it "has players that are short term and long term" do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.roster).to eq([player_1, player_2, player_3, player_4])
+    expect(team.player_count).to eq(4)
+    expect(team.long_term_players).to eq([player_1, player_3])
+    expect(team.short_term_players).to eq([player_2, player_4])
+  end
+
+  it "has value" do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.total_value).to eq(85200000)
+    expect(team.details).to be_instance_of(Hash)
+    expect(team.details).to eq({"total_value" => 85200000, "player_count" => 4})
+  end
+
+  it "has value" do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.average_cost_of_player).to eq("$21,300,000")
+    expect(team.players_by_last_name).to eq("DeNunez, McClennan, Palledorous, Porter")
+  end
 end
